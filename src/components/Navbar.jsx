505d2050f5b56e66a0185d394d1415106ce95596
radzhiv <radzhivkrishna@gmail.com>
2024-08-27 07:31:54 +0530

    feat. Added github link for contribution

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import { FaGithub } from "react-icons/fa";
 
 const Navbar = () => {
   return (
     <div>
-      <div className="my-5 p-3 border border-gray-200 rounded-md">
+      <div className="my-5 p-3 border border-gray-200 rounded-md flex justify-between items-center">
         <span className="leading-none">
-        <h1 className="text-2xl font-semibold leading-none bg-gradient-to-br from-gray-300 via-black text-transparent bg-clip-text">PennWise</h1>
-        <p className="text-xs">manage expense in clicks</p>
+          <h1 className="text-2xl font-semibold leading-none bg-gradient-to-br from-gray-300 via-black text-transparent bg-clip-text">
+            PennWise
+          </h1>
+          <p className="text-xs">manage expense in clicks</p>
+        </span>
+        <span>
+            <a href="https://github.com/radzhiv25/expense-tracker">
+            <FaGithub className="size-8 hover:text-gray-500" />
+            </a>
         </span>
       </div>
     </div>
